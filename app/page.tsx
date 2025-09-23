4fe57b0ac09e24b169220681bdbe32bbe9b4254e
marwan <marwan02shara@gmail.com>
2025-09-23 18:43:08 -0400

    Reapply "add ui components"
    This reverts commit 6325e017463a3e29e3cf62c69be4dbe457528904.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import "./main.css";
-import {Button} from '@heroui/button';
+import PrimaryButton from './components/button';
+import Textarea from './components/textarea';
+import ReadOnlyTextArea from "./components/readOnlyTextArea";
 
+ 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">
+    <div className="flex flex-col gap-24">
+      <h1 className="text-3xl font-bold self-center">
         Tailor your Resume with AI
       </h1>
-      <div className="flex">
-        <Textarea></Textarea>
-        <ReadOnlyTextArea></ReadOnlyTextArea>
+      <div className="flex justify-around gap-8">
+          <Textarea></Textarea>
+          <ReadOnlyTextArea></ReadOnlyTextArea>
       </div>
       <PrimaryButton></PrimaryButton>
     </div>
