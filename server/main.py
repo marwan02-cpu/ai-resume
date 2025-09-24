a915a115265b690087110c85057f94ff4d5ed04a
marwan <marwan02shara@gmail.com>
2025-09-24 14:11:25 -0400

    Add prompt endpoint for api

--- server/main.py
+++ server/main.py
@@ -1,13 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Prompt(BaseModel):
+    content: str
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+@app.post("/prompt/")
+async def create_item(prompt: Prompt):
+    return prompt
